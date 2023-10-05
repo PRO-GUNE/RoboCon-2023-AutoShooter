46a9f6b33298fca700534a3e2697226b18528102
PRO-GUNE <chathuragunasekera@gmail.com>
2023-10-05 17:18:19 +0530

    changed to detect spheres

--- poleDetect.py
+++ poleDetect.py
@@ -11,8 +11,8 @@ cv2.namedWindow('Kinect Color Stream', cv2.WINDOW_AUTOSIZE)
 cv2.namedWindow('Hand Detection', cv2.WINDOW_AUTOSIZE)
 
 # Color thresholds
-lower_color_bound = np.array([0,50,50], dtype=np.uint8)
-upper_color_bound = np.array([20,255,255], dtype=np.uint8)
+lower_color_bound = np.array([0,0,200], dtype=np.uint8)
+upper_color_bound = np.array([180,30,255], dtype=np.uint8)
 
 while True:
     # Capture frames
@@ -23,31 +23,31 @@ while True:
     color_image = cv2.cvtColor(rgb_frame, cv2.COLOR_RGB2BGR)
     depth_image = depth_frame.astype(np.uint64)
 
-    # Apply color segmentation to detect hand
+    # Apply color segmentation to detect ball
     hsv_frame = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv_frame, lower_color_bound, upper_color_bound)
 
-    # Find contours of the hand
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Find circular objects using Hough Circle Transform
+    circular_objects = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, dp=1, minDist=50, param1=50, param2=30, minRadius=50, maxRadius=150)
 
     # Iterate through the contours and calculate the center and depth
-    for contour in contours:
+    if circular_objects is not None:
         # Filter small contours
-        if cv2.contourArea(contour) > 100:
-            # Get bounding rectangle and its center
-            x,y,w,h = cv2.boundingRect(contour)
-            center_x = x+w // 2
-            center_y = y+h // 2
+        circular_objects = np.uint16(np.around(circular_objects))
+        for circle in circular_objects[0, :]:
+            center = (circle[0], circle[1])
+            radius = circle[2]
+            
+            if center[0] < 640 and center[1] < 480:
+                # Get the corresponding depth from the depth frame
+                depth = depth_image[center[1], center[0]]
 
-            # Get the corresponding depth from the depth frame
-            depth = depth_image[center_y, center_x]
+                # Display depth above the bounding box (in mm)
+                distance = trasformDistance(depth)
+                cv2.putText(color_image, f"Distance: {distance: .2f} mm", (center[0], center[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
 
-            # Display depth above the bounding box (in mm)
-            distance = trasformDistance(depth)
-            cv2.putText(color_image, f"Distance: {distance: .2f} mm", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
-
-            # Draw bounding box around the hand
-            cv2.rectangle(color_image, (x,y), (x+w, y+h), (0,255,0), 2)
+                # Draw the bounding circle
+                cv2.circle(color_image, center, radius, (0,255,0), 2)
 
     # Display frames
     cv2.imshow('Kinect Color Stream', color_image)
